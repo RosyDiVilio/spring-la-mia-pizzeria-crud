649455df9ff5bbdf5f6d7c8c6520b500f8f2d9f5
RosyDiVilio <165782561+RosyDiVilio@users.noreply.github.com>
2024-09-04 11:25:09 +0200

    add: pizzeria con index e show

---
+++ src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
@@ -0,0 +1,43 @@
+package org.lessons.java.pizzeria.controller;
+
+import java.util.List;
+
+import org.lessons.java.pizzeria.model.Pizza;
+import org.lessons.java.pizzeria.repo.PizzaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//controller = prende dati dal model e li rendere disponibili alla view
+@Controller
+@RequestMapping("/pizzas")
+public class PizzaController {
+	
+	//repository field con autowired per D.I.
+	@Autowired
+	private PizzaRepository repo;
+	
+	@GetMapping
+	public String index(Model model) {
+		
+		//creo lista pizza e dopo prendo i dati dal DB
+		List<Pizza> pizzaList = repo.findAll();
+		
+		//inserisco dati nel modello
+		model.addAttribute("pizzas", pizzaList);
+		return "pizzeria/index";
+	}
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") Integer id, Model model) {
+		Pizza pizza = repo.findById(id).get();
+		model.addAttribute("pizza", pizza);
+		
+		return "pizzeria/show";
+	}
+	
+	
+}
