cd7ef04d969758d0ee48265ad41c9ac7b1b2d461
RosyDiVilio <165782561+RosyDiVilio@users.noreply.github.com>
2024-09-12 18:12:03 +0200

    add: update e delete

--- src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
+++ src/main/java/org/lessons/java/pizzeria/controller/PizzaController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.validation.Valid;
 
@@ -44,6 +45,8 @@ public class PizzaController {
 		return "pizzeria/show";
 	}
 	
+	//CREATE
+	
 	@GetMapping("/create")
 	public String create(Model model) {
 		model.addAttribute("pizza", new Pizza());
@@ -51,18 +54,77 @@ public class PizzaController {
 		return "pizzeria/create";
 	}
 	
+	//STORE
+	
 	@PostMapping("/create")
 	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, 
-			BindingResult bindingResult,
-			Model model) {
+			            BindingResult bindingResult,
+			            Model model,
+			            RedirectAttributes attributes) {
 		
 		if(bindingResult.hasErrors()) {
-			return "/pizzas/create";
+			return "/pizzeria/create";
 		} else {
 			repo.save(formPizza);
+			attributes.addFlashAttribute("successMessage", formPizza.getName() + " has been created successfully");
 			return "redirect:/pizzas";
 		}
 		
 	}
 	
+	//EDIT => una GET che restituisce al chiamante un form con i dati della risorsa richiesta
+	
+	 @GetMapping("/edit/{id}")
+	 public String edit(@PathVariable("id") Integer id, Model model) {
+		 
+		 //trovo la pizza
+		// Pizza pizzaToEdit = repo.findById(id).get();
+		 
+		 //la insierisco nel model
+		// model.addAttribute("pizza", pizzaToEdit); 
+		 
+		 model.addAttribute("pizza", repo.findById(id).get());
+		 
+		 //restituisco la view con il model inserito
+		 return "/pizzeria/edit";
+	 }
+	
+	//UPDATE => una POST che aggiorna nel DB i nuovi dati inseriti, se corretti, della risorsa
+	
+	 @PostMapping("/edit/{id}")
+	 public String update(@Valid @ModelAttribute("pizza") Pizza updatedFormPizza, 
+				          BindingResult bindingResult,
+				          Model model,
+				          RedirectAttributes attributes) {
+		 
+		 //se ci sono errori nel from
+		 if(bindingResult.hasErrors()) {
+		    //ritorna nel form e mostra gli errori	 
+			return "/pizzas/edit";
+		 } else {
+		      //altriemnti prendi la repo e aggiorna la pizza con i nuovi dati	 
+			 repo.save(updatedFormPizza);
+		     //ridireziona l'utente alla index delle pizze
+			 attributes.addFlashAttribute("successMessage", updatedFormPizza.getName() + " has been updated successfully");
+			 return "redirect:/pizzas";
+		 }
+	 }
+	 
+	 //DELETE
+	 
+	 @PostMapping("/delete/{id}") 
+		 public String delete(@PathVariable("id") Integer id,
+				              RedirectAttributes attributes) {
+		 
+		 //prendi la repo e dopo aver trovato la pizza tramite l'id, cancellalo dal DB
+		 repo.deleteById(id);
+			 
+		 //ridireziona l'utente alla index delle pizze
+		     attributes.addFlashAttribute("successMessage", "Pizza with id " + id + " has been deleted successfully");
+			 return "redirect:/pizzas";
+		 }
+		 
+		
+		 
 }
+	
